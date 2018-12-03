b501ca3be53ad9f5bc31544e671ddc5c51285198
Will Smith <lwilsonsmith@gmail.com>
2018-12-03 07:08:43 -0500

    Remove fonts in favor of png logo

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import logo from '../images/logo.svg';
+import logo from '../images/logo.png';
 
 const HeaderTag = styled.header`
   display: flex;
