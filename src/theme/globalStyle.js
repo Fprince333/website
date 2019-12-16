27bae2e5386af758a8bbe4b385646a41b35e390b
Will Smith <lwilsonsmith@gmail.com>
2019-12-15 20:08:44 -0500

    Fix font paths

--- src/theme/globalStyle.js
+++ src/theme/globalStyle.js
@@ -1,16 +1,21 @@
 import { createGlobalStyle } from "styled-components";
+import trueLiesEot from '../fonts/TrueLies.eot';
+import trueLiesWoff2 from '../fonts/TrueLies.woff2';
+import trueLiesWoff from '../fonts/TrueLies.woff';
+import trueLiesTtf from '../fonts/TrueLies.ttf';
+import trueLiesSvg from '../fonts/TrueLies.svg';
 
 const GlobalStyle = createGlobalStyle`
   @import url(https://fonts.googleapis.com/css?family=Source+Sans+Pro:300italic,600italic,300,600);
   @import url(https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.css);
   @font-face {
     font-family: 'TrueLies';
-    src: url('fonts/TrueLies.eot');
-    src: url('fonts/TrueLies.eot') format('embedded-opentype'),
-         url('fonts/TrueLies.woff2') format('woff2'),
-         url('fonts/TrueLies.woff') format('woff'),
-         url('fonts/TrueLies.ttf') format('truetype'),
-         url('fonts/TrueLies.svg#TrueLies') format('svg');
+    src: url(${trueLiesEot});
+    src: url(${trueLiesEot}) format('embedded-opentype'),
+         url(${trueLiesWoff2}) format('woff2'),
+         url(${trueLiesWoff}) format('woff'),
+         url(${trueLiesTtf}) format('truetype'),
+         url(${trueLiesSvg}) format('svg');
         }
 
   *, :after, :before {
@@ -30,7 +35,7 @@ const GlobalStyle = createGlobalStyle`
 
   h1,
   h2 {
-    font-family: 'TrueLies', Source Sans Pro, sans-serif;
+    font-family: TrueLies, Source Sans Pro, sans-serif;
   }
 
   iframe {
